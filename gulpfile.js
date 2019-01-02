d128a9d2a246c94ee5facf44690859908e6d9be8
zhenzhen <@2868382033.com>
2019-01-02 09:20:42 +0800

    起服务完成

--- gulpfile.js
+++ gulpfile.js
@@ -12,11 +12,17 @@ let babel = require("gulp-babel");
 
 let bCss = require("gulp-clean-css");
 
+let fs = require("fs");
+
+let path = require("path");
+
+let url = require("url");
 
 //编译scss
 gulp.task('sass', function() {
     return gulp.src("./src/scss/*.scss")
         .pipe(sass())
+        .pipe(bCss())
         .pipe(gulp.dest('./src/css'));
 });
 
@@ -24,3 +30,25 @@ gulp.task('sass', function() {
 gulp.task("watch", function() {
     return gulp.watch("./src/scss/*.scss", gulp.series("sass"));
 })
+
+//起服务
+gulp.task('webserver', function() {
+    gulp.src('src')
+        .pipe(server({
+            livereload: true,
+            directoryListing: true,
+            middleware: function(req, res, next) {
+                let pathname = url.parse(req.url).pathname;
+                if (pathname == "/favicon.ico") {
+                    res.end("");
+                    return
+                } else {
+                    pathname = pathname == "/" ? "index.html" : pathname;
+                    res.end(fs.readFileSync(path.join(__dirname, "src", pathname)))
+                }
+            }
+        }));
+});
+
+
+gulp.task("default", gulp.series("sass", "webserver", "watch"))
